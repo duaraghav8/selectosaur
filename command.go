f6996e29ffb45b7e406bd6d5f8ce374c578c15e2
duaraghav8 <12758282+duaraghav8@users.noreply.github.com>
2021-10-28 16:26:28 +0530

    top-level logic

--- command.go
+++ command.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
+	"io"
+	"os"
 )
 
 var command = &cobra.Command{
@@ -26,11 +30,40 @@ func init() {
 }
 
 func commandHandler(cmd *cobra.Command, args []string) error {
-	qpf, _ := cmd.Flags().GetString("qp")
 	wc, _ := cmd.Flags().GetInt("worker-count")
+	if wc < 1 {
+		return errors.New("worker count cannot be less than 1")
+	}
+	// TODO: wc upper limit?
+
+	qpFile, _ := cmd.Flags().GetString("qp")
+	f, err := os.Open(qpFile)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %v", qpFile, err)
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	// TODO: ensure that we skip first row (which contains field names)
+	for {
+		rec, err := reader.Read()
+		if err == io.EOF {
+			// no more query parameters left
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read CSV record: %v", err)
+		}
+
+		qp, err := newQueryParam(rec)
+		if err != nil {
+			return fmt.Errorf("failed to parse query param CSV record %v: %v", rec, err)
+		}
+
+		fmt.Println(qp)
+	}
 
 	// create a thread-safe datastore object to create sql queries & interact with timescaledb
-	// validations for qpf & wc
 	// create worker pool based on count
 	// load query params 1-by-1 (stream)
 	//
@@ -42,7 +75,5 @@ func commandHandler(cmd *cobra.Command, args []string) error {
 	// calculate total no. of queries, total time across all queries
 	// calculate query times: min, med, avg, max
 
-	fmt.Println(qpf)
-	fmt.Println(wc)
 	return nil
 }
---
+++ query_parameter.go
@@ -0,0 +1,18 @@
+package main
+
+type QueryParameter struct {
+	Hostname           string
+	StartTime, EndTime string
+}
+
+// newQueryParam takes a string slice of 3 items:
+// [hostname, start_time, end_time]
+// It validates the data and returns a query param object in case of no errors.
+func newQueryParam(rec []string) (*QueryParameter, error) {
+	// validate all values
+	return &QueryParameter{
+		Hostname:  rec[0],
+		StartTime: rec[1],
+		EndTime:   rec[2],
+	}, nil
+}
